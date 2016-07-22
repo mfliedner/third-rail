52943ff9694b0285858622f04448e61a970b1ebb
Moritz Fliedner <moritz@stanfordalumni.org>
2016-07-22 15:38:40 -0700

    error styling

--- lib/show_exceptions.rb
+++ lib/show_exceptions.rb
@@ -37,18 +37,29 @@ class ShowExceptions
     @exception.backtrace
   end
 
-  def source_code # pull out problematic code, and preceding / following lines
-    pattern = Regexp.new("^(?<path>.*rb):(?<line_num>\\d+).*$")
-    capture = pattern.match(@exception.backtrace[0])
-    path = capture[:path]
-    line_num = capture[:line_num].to_i - 1
-    file = File.readlines(path)
-
-    file[line_num - 3..line_num + 3]
+  def locate_exception
+    stack_trace.first.split(":")
   end
 
-  def exception_msg # pull out error message
-    @exception.message
+  def exception_line_number
+    locate_exception[1].to_i
+  end
+
+  def source_file_name
+    locate_exception.first
+  end
+
+  def source_view
+    file = File.readlines(source_file_name)
+    start_idx = [0, exception_line_number - 3].max
+    end_idx = [file.length-1, exception_line_number + 3].min
+    file[start_idx..end_idx].map.with_index do |line, i|
+      "#{start_idx+i+1}: #{line}"
+    end
+  end
+
+  def exception_message
+    "#{@exception.class}: #{@exception.message}"
   end
 
 end
