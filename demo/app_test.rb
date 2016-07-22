b7c28949df9e8edb32f4212a4fb3d6fc8d7fd90e
Moritz Fliedner <moritz@stanfordalumni.org>
2016-07-22 12:35:47 -0700

    extend demo for router and exceptions-handler

--- demo/app_test.rb
+++ demo/app_test.rb
@@ -5,11 +5,8 @@ require_relative '../lib/router'
 require_relative '../lib/controller_base'
 require_relative '../lib/show_exceptions'
 
-# NB: "Hello World" app
-
 class Controller < ControllerBase
   def index
-    render_content("HELLO WORLD", "text/html")
   end
 end
 
@@ -17,6 +14,8 @@ router = Router.new
 router.draw do
   get Regexp.new("^/$"), Controller, :index
 end
+
+# NB: renders the static root page of Buffon's Needle
 
 simple_app = Proc.new do |env|
   req = Rack::Request.new(env)
